011ca1aa0dae3942a5f97c867786c0d664936313
reggieroby <reggie.a.roby@gmail.com>
2015-02-22 11:24:38 -0600

    refining angular map

---
+++ battleofthecode/public_html/actions.php
@@ -0,0 +1,14 @@
+<?php
+require 'dbcommands.php';
+
+if($_POST["table"]="person"){
+    if($_POST["crud"]="create"){
+        if(empty(getUserID($_POST["email"]))){
+            createUser($_POST["email"],$_POST["password"]);
+        }
+    }else if($_POST["crud"]="read"){
+        createUser($_POST["email"],$_POST["pass"]);
+    }
+}if($_POST["table"]="visited"){
+    
+}
